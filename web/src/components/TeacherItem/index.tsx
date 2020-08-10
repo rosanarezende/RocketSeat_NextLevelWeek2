e23bb5f261d72c1aa5fe90e37ec68551d86fa13e
Rosana Rezende <rezende_rosana@hotmail.com>
2020-08-09 21:02:53 -0300

    dia3: conexão de front e back

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,36 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import { Teacher } from '../../pages/TeacherList';
+
 import './styles.css'
 
-const TeacherItem: React.FC = () => {
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://scontent.fvix2-1.fna.fbcdn.net/v/t1.0-9/15181290_1274938989233000_6827837919154649036_n.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=5COtuSHLN0UAX9HdIOE&_nc_ht=scontent.fvix2-1.fna&oh=95d940b8499448d13727322caee1a288&oe=5F4E4D7A" alt="Rosana Rezende" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Rosana Rezende</strong>
-          <span>React</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        <br /><br />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem delectus quod officiis, quisquam a ullam. Excepturi iure perferendis illo error delectus, cupiditate tempora exercitationem quis odio voluptatum reiciendis culpa assumenda!
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 20,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,42 +1,98 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import api from '../../services/api';
 
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  subject: string;
+  cost: number;
+  name: string;
+  whatsapp: string;
+  bio: string;
+}
 
 const TeacherList: React.FC = () => {
+  const [subject, setSubject] = useState('')
+  const [week_day, setWeekDay] = useState('')
+  const [time, setTime] = useState('')
+
+  const [teachers, setTeachers] = useState([])
+
+  const searchTeachers = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    })
+
+    setTeachers(response.data)
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Essas são as mentoras disponíveis">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Linguagem / Framework</label>
-            <input type="text" id="subject"/>
-          </div>
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Linguagem/Framework"
+            options={[
+              { value: 'JavaScript', label: 'JavaScript' },
+              { value: 'Typescript', label: 'Typescript' },
+              { value: 'Java', label: 'Java' },
+              { value: 'Ruby', label: 'Ruby' },
+              { value: 'Python', label: 'Python' },
+              { value: 'SQL', label: 'SQL' }
+            ]}
+            value={subject}
+            onChange={e => setSubject(e.target.value)}
+          />
 
-          <div className="input-block">
-            <label htmlFor="cost">Valor</label>
-            <input type="text" id="cost"/>
-          </div>
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+            value={week_day}
+            onChange={e => setWeekDay(e.target.value)}
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={e => setTime(e.target.value)}
+          />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <button type="submit">
+            Buscar
+          </button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />
+        })}
       </main>
     </div>
   );
